66390763dd4a7159be8dc0d640e768d50eebc921
Achilles Rasquinha <achillesrasquinha@gmail.com>
2019-10-24 18:07:44 -0500

    Create Regulator class

--- src/cc/model/__init__.py
+++ src/cc/model/__init__.py
@@ -1,5 +1,6 @@
 # imports - module imports
-from cc.model.model.base             import Model
-from cc.model.model.boolean.model    import BooleanModel
-from cc.model.model.boolean.species  import Species
-from cc.model.user                   import User
+from cc.model.model.base                import Model
+from cc.model.model.boolean.model       import BooleanModel
+from cc.model.model.boolean.species     import Species
+from cc.model.model.boolean.regulator   import Regulator
+from cc.model.user                      import User
---
+++ src/cc/model/model/boolean/regulator.py
@@ -0,0 +1,4 @@
+from cc.model.resource import Resource
+
+class Regulator(Resource):
+    pass
